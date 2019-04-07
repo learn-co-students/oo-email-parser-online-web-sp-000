bb910aa456814859a7e43ab9f0315a11de27b516
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 04:39:47 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,21 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailParser
+  attr_accessor :list_of_emails, :string
+  
+  def initialize(string)
+    @string = string
+    
+  end
+  
+  def parse
+    @list_of_emails = @string.split(" ")
+    @list_of_emails = @list_of_emails.collect {|email| email.chomp(",")}
+    unique_array = []
+    @list_of_emails.each do |email|
+      unique_array << email if !unique_array.include?(email)
+    end
+    unique_array
+  end
+end
