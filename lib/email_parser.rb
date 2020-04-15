61ce6ae6776e9cd105aaa11b1cc54f433abce807
Joseph Kao <josephtkao@gmail.com>
2020-04-15 16:13:30 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,10 +3,15 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-
+#I don't know how to use the OR function in Regex so remove commas, then spaces
   def parse
     @email.split(/,/).join.split(" ").uniq
   end
+#from the solution - this is how you implement OR in Regex
+#  def parse
+#    emails.split(/, | /).uniq
+#  end
+
 
   def initialize(email)
     @email = email
