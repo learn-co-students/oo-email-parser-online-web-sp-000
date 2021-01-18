c7e41e075ccd11602bb021ce10663dfd487b6403
remote-torrent-6747 <abbyc@sky.com>
2021-01-18 13:21:02 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,18 +3,18 @@ class EmailAddressParser
 
   attr_accessor :email_addresses
 
-  def initalize (email_addresses)
-    #separated email addresses
+  def initialize(email_addresses)
     @email_addresses = email_addresses
-    formatted_list = email_addresses.split(/[,| ]/)
-    return formatted_list
   end
 
   def parse
     #separated email addresses, but no duplicates
-    @email_addresses = email_addresses
-    formatted_list = email_addresses.split(/[,| ]/)
+    formatted_list = @email_addresses.split(/(?:, )|[, ]/)
     return formatted_list.uniq
   end
 
 end
+
+#regex explained: asking for (a comma and a space) OR [a comma or a space]
+#when using (), it will provide a matching group which adds it onto the array
+#in this case, we want to ignore that so adding ?: tells it that it is not matching
